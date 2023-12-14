e942f6f92ea0d716fc8de97a90278b29b9158e22
Cecilia <ccchimento@gmail.com>
2023-12-13 17:08:38 -0700

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  const ul = document.querySelector('ul');
+
+  function handleSubmit(e)
+  {
+    e.preventDefault();
+    //console.log(e.target.name);
+    const newTask = document.querySelector('#new-task-description').value;
+    const li = document.createElement('li');
+    li.textContent = newTask;
+    ul.appendChild(li);
+
+  }
+  form.addEventListener('submit', (e) => handleSubmit(e))
 });
