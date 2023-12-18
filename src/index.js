ed515eef6b5e5ca08e2db6dd413216a2ad7b81a7
Cecilia <ccchimento@gmail.com>
2023-12-18 15:23:44 -0700

    paired programming edit

--- src/index.js
+++ src/index.js
@@ -1,16 +1,55 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector('form');
-  const ul = document.querySelector('ul');
+  const form = document.getElementById("create-task-form");
+  const list = document.getElementById("tasks");
+  
+  const dropdown = document.createElement('select');
+  const low = document.createElement('option');
+  low.value = "low";
+  low.textContent = "low";
+  const medium = document.createElement('option');
+  medium.value = "medium";
+  medium.textContent = "medium";
+  const high = document.createElement('option');
+  high.value = "high";
+  high.textContent = "high";
+  dropdown.appendChild(low);
+  dropdown.appendChild(medium);
+  dropdown.appendChild(high);
+  form.appendChild(dropdown);
 
-  function handleSubmit(e)
-  {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    //console.log(e.target.name);
-    const newTask = document.querySelector('#new-task-description').value;
-    const li = document.createElement('li');
-    li.textContent = newTask;
-    ul.appendChild(li);
+    const newTask = document.getElementById('new-task-description').value;
+    const newItem = document.createElement('li');
+    newItem.textContent = newTask;
+    const button = document.createElement('button');
+    button.textContent = "Delete";
 
-  }
-  form.addEventListener('submit', (e) => handleSubmit(e))
+    const priority = dropdown.value;
+    if (priority == "low"){
+      newItem.style.color = "green"
+    }
+    else if (priority == "medium"){
+      newItem.style.color = "yellow"
+    }
+    else if (priority == "high"){
+      newItem.style.color = "red"
+    }
+    
+    list.appendChild(newItem)
+    list.appendChild(button)
+
+    button.addEventListener('click', () => {
+      newItem.remove();
+      button.remove();
+    })
+  })
+
 });
+
+/*
+Deliverables:
+1) should be able to type a task into the input field
+2) should be able to click submit button
+3) task should appear in the DOM
+*/
